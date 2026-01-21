9211bc449ba8a0739955df2fed43663e3ee09d36
paxman234 <sav-fernandes@outlook.com>
2026-01-21 12:38:15 +0000

    read the .env and set the variables in process.env. target createClient to set the variable of the value to initialize supabase.

--- backend/provider/supabase.js
+++ backend/provider/supabase.js
@@ -1,8 +1,8 @@
-
 // Import `dotenv` package and call the `config()` function to access the environment variables set within the `.env` file. 
-
+require("dotenv").config();
 
 // Abstract the `createClient` functionality from the `supabase` package. 
+const { createClient } = require("@supabase/supabase-js");
 
 
 // Create two const variables `supabaseURL` and `supabaseSecret` with the value of the environment variables you created.
